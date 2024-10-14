3227a05dbea9dbcf8c9bdc2fc7f054bfd5042850
v132327_ual <victor.zamora@globant.com>
2024-10-14 15:15:01 -0600

    new scripts

--- EKS-tf-jenkins/terraform/provider.tf
+++ EKS-tf-jenkins/terraform/provider.tf
@@ -1,8 +1,8 @@
 locals {
-  region = "us-east-1"
+  region = "us-east-2"
   name   = "amonkincloud-cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["us-east-2a", "us-east-2b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
@@ -12,5 +12,5 @@ locals {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
