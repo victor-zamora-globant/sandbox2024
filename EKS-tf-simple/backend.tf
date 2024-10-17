67211a0ec6cd0986f5d58aa371f4d3a1f3cb17f4
v132327_ual <victor.zamora@globant.com>
2024-10-17 11:50:18 -0600

    new scripts

---
+++ EKS-tf-simple/backend.tf
@@ -0,0 +1,13 @@
+
+
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "vic-sandbox-bucket-2024"
+    key    = "tf/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
